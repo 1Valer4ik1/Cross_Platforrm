da0c1fb24441eb86b0df3f5c235ba156e03d1679
1Valer4ik1 <valera05744@gmail.com>
2024-12-20 00:50:44 +0200

    Add files via upload

---
+++ LabsLibrary/LabRunner.cs
@@ -0,0 +1,20 @@
+﻿namespace LabsLibrary
+{
+    public class LabRunner
+    {
+        public void RunLab1(string inputFilePath, string outputFilePath)
+        {
+            
+        }
+
+        public void RunLab2(string inputFilePath, string outputFilePath)
+        {
+            
+        }
+
+        public void RunLab3(string inputFilePath, string outputFilePath)
+        {
+           
+        }
+    }
+}
